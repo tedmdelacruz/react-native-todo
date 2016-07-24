fb33b6a30dabd976f11f03733516a17d5ae3e4f0
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2016-07-24 12:20:24 +0800

    Directory restructure. Moved all app code to /src folder

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
  import { createStore } from 'redux'
  import { Provider } from 'react-redux'
 
-import { reducer } from '../redux/reducer'
+import { reducer } from '../reducer'
 import Todo from './Todo'
 
 const store = createStore(reducer)
